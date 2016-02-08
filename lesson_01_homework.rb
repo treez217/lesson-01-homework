5c8332c7b4e387c73b78e7918169fb5e0615bcbe
Kareem Grant <kareem@getuserwise.com>
2016-02-08 17:05:52 -0500

    update lesson 01 homework

---
+++ lesson_01_homework.rb
@@ -0,0 +1,38 @@
+# Code Reading
+
+def full_name(first_name, last_name, title)
+  # Example comment line
+
+  name = nil
+
+  if title && first_name && last_name
+    name = title + " " + first_name + " " + last_name
+  elsif title && last_name
+    name = title + " " + last_name
+  elsif first_name && last_name
+    name = first_name + " " + last_name
+  elsif first_name
+    name = first_name
+  else
+    raise "Oh no, that doesn't look like a name"
+  end
+
+  return name
+end
+
+
+# Methods
+
+
+## Enter your code for your 'add' method below
+
+
+
+
+## Enter the code for you 'join_strings' method below
+
+
+
+
+
+## Enter your 'old_enough_to_vote?' method below
