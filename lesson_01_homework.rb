c1001dad7bb04225f33a938b807f10ec02cc2d4f
Derrick Chou <treez217@gmail.com>
2016-02-14 12:21:12 -0500

    added comments and fixed broken line of code

--- lesson_01_homework.rb
+++ lesson_01_homework.rb
@@ -1,22 +1,30 @@
 # Code Reading
 
+#defining a method for full name that includes title.  Maybe used for a name entry field 
 def full_name(first_name, last_name, title)
+
   # Example comment line
 
+  # name is not defined yet and left as blank
   name = nil
 
+  #'AND'logic is used here. if a title and first name and last name are given then return in that order with a space between text
   if title && first_name && last_name
     name = title + " " + first_name + " " + last_name
+  #otherwise look for the given title and given last name and return in that order with space    
   elsif title && last_name
     name = title + " " + last_name
+  #otherwise look for the given first name and given last name and return in that order with space    
   elsif first_name && last_name
     name = first_name + " " + last_name
+  #otherwise look for only first name given and return only first name
   elsif first_name
     name = first_name
+  #otherwise lastly if all previous arguments fail then it's not possible for it to be a name and will return the error message specified.  
   else
     raise "Oh no, that doesn't look like a name"
   end
-
+  #argument to return that is currently specified to 'nil'
   return name
 end
 
@@ -26,7 +34,8 @@ def full_name(f, l)
   puts f + l
 end
 
-puts full_nam("bubba", "chuck")
+#there was an error with the spelling of name.  i changed it from full_nam to full_name
+puts full_name("bubba", "chuck")
 
 
 # Coding
